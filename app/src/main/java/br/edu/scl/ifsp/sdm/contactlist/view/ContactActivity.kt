07acb9d8f91eff0472bf15b618c37b1a6a447fab
unknown <viniciusldegan@gmail.com>
2023-11-28 13:53:39 -0300

    first commit

---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/ContactActivity.kt
@@ -0,0 +1,66 @@
+package br.edu.scl.ifsp.sdm.contactlist.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.GONE
+
+import br.edu.scl.ifsp.sdm.contactlist.R
+import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityContactBinding
+import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Contact
+
+class ContactActivity : AppCompatActivity() {
+    private val acb: ActivityContactBinding by lazy {
+        ActivityContactBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(acb.root)
+
+        setSupportActionBar(acb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.contact_details)
+
+        val receivedContact = intent.getParcelableExtra<Contact>(EXTRA_CONTACT)
+        receivedContact?.let { received->
+            val viewContact = intent.getBooleanExtra(EXTRA_VIEW_CONTACT, false)
+            with(acb){
+                if (viewContact) {
+                    nameEt.isEnabled = false
+                    adressEt.isEnabled = false
+                    phoneEt.isEnabled = false
+                    emailEt.isEnabled = false
+                    saveBt.visibility = GONE
+                }
+            }
+            with(acb){
+                nameEt.setText(received.name)
+                adressEt.setText(received.adress)
+                phoneEt.setText(received.phone)
+                emailEt.setText(received.email)
+            }
+        }
+
+
+        with(acb){
+            saveBt.setOnClickListener{
+                val contact = Contact(
+                    id = receivedContact?.id?:hashCode(),
+                    name = nameEt.text.toString(),
+                    adress = adressEt.text.toString(),
+                    phone = phoneEt.text.toString(),
+                    email = emailEt.text.toString()
+
+                )
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_CONTACT, contact)
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/MainActivity.kt
@@ -0,0 +1,126 @@
+package br.edu.scl.ifsp.sdm.contactlist.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView.AdapterContextMenuInfo
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.edu.scl.ifsp.sdm.contactlist.R
+import br.edu.scl.ifsp.sdm.contactlist.adapter.ContactRvAdapter
+import br.edu.scl.ifsp.sdm.contactlist.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Constant.EXTRA_VIEW_CONTACT
+import br.edu.scl.ifsp.sdm.contactlist.model.Contact
+
+class MainActivity : AppCompatActivity(), OnContactClickListener {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    //Data Source
+    private val contactlist: MutableList<Contact> = mutableListOf()
+
+    //Adapter
+
+    private val contactAdapter: ContactRvAdapter by lazy {
+       ContactRvAdapter(contactlist, this)
+    }
+
+    private lateinit var carl: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.contact_list)
+
+        carl =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val contact = result.data?.getParcelableExtra<Contact>(EXTRA_CONTACT)
+                    contact?.also { newOrEditedContac ->
+                        if (contactlist.any { it.id == newOrEditedContac.id }) {
+                            val position = contactlist.indexOfFirst { it.id == newOrEditedContac.id }
+                            contactlist[position] = newOrEditedContac
+                            contactAdapter.notifyItemChanged(position)
+                        } else {
+                            contactlist.add(newOrEditedContac)
+                            contactAdapter.notifyItemInserted(contactlist.lastIndex)
+                        }
+                    }
+                }
+            }
+        fillContacts()
+
+        amb.contactsRv.adapter = contactAdapter
+        amb.contactsRv.layoutManager = LinearLayoutManager(this)
+
+      }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.addContactMi -> {
+                carl.launch(Intent(this, ContactActivity::class.java))
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    override fun onRemoveContactMenuItemClick(position: Int) {
+        contactlist.removeAt(position)
+        contactAdapter.notifyItemRemoved(position)
+        Toast.makeText(this, getString(R.string.contact_removed), Toast.LENGTH_SHORT).show()
+
+
+    }
+    override fun onEditContactMenuItemClick(position: Int) {
+        carl.launch(Intent(this, ContactActivity::class.java).apply {
+            putExtra(EXTRA_CONTACT, contactlist[position])
+        })
+    }
+
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+    override fun onContactClick(position: Int) {
+        Intent(this, ContactActivity::class.java).apply {
+            putExtra(EXTRA_CONTACT, contactlist[position])
+            putExtra(EXTRA_VIEW_CONTACT, true)
+        }.also {
+            startActivity(it)
+        }
+    }
+    private fun fillContacts(){
+        for (i in 1..10){
+            contactlist.add(
+                Contact(
+                    i,
+                    "Nome $i",
+                    "Endereço $i",
+                    "Telefone $i",
+                    "Email $i"
+                )
+            )
+        }
+    }
+}
